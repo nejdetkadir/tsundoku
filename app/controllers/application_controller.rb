2d3336f2fd39787cbb2e7792f5c8f20ffcbe6671
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-13 20:45:18 +0300

    Create redirect if user signed in functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || pages_profile_path
+  end
+
+  def check_signed_in
+    redirect_to pages_profile_path if signed_in?
+  end
+
   protected
 
     def configure_permitted_parameters
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:profile]
+  before_action :check_signed_in, only: [:welcome]
 
   def welcome
   end
