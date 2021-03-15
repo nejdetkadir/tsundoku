c1c1d2b816f07eb66f76c9c8726d48945eb4f042
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-15 15:52:28 +0300

    Add follow button to profile page of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'pages#welcome'
+
   resources :books, except: [:index]
   resources :book_comments, only: [:create, :destroy]
-  root to: 'pages#welcome'
+
   devise_for :users
 
   get '/u/:id', to: 'users#show', as: 'user_profile'
   get '/profile', to: 'pages#profile', as: 'pages_profile'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
